b02c95b3a611f09b7bdc2663860b71be36668fb3
KolpakovaAnastasia <aik20112011@mail.ru>
2017-10-16 21:03:08 +0300

    исправлено x2 + lesson4

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -84,10 +84,9 @@ fun digitNumber(n: Int): Int {
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
 fun fib(n: Int): Int {
-    if (n == 1 || n == 2) return 1
+    if ((n == 1) || (n == 2)) return 1
     else return fib(n - 1) + fib(n - 2)
 }
-
 
 /**
  * Простая
